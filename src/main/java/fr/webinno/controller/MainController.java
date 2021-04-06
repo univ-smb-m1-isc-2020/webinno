cdb9e601f1b183201807ce55cee4305bca1e3676
benjaminglt1 <benjamin.gallet@etu.univ-savoie.fr>
2021-04-06 15:06:42 +0200

    add connexion

--- src/main/java/fr/webinno/controller/MainController.java
+++ src/main/java/fr/webinno/controller/MainController.java
@@ -1,9 +1,36 @@
 package fr.webinno.controller;
 
-import fr.webinno.*;
+import fr.webinno.domain.Resolution;
+import fr.webinno.service.ResolutionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Controller
 public class MainController {
+    private final ResolutionService resolutionService;
+    private List<Resolution> resolutionService.
 
+    @Autowired
+    public MainController(ResolutionService resolutionService) {
+        this.resolutionService = resolutionService;
+    }
+
+    public List<String> resolutions(){
+        List<String> l = resolutionService.getAllResolutions().stream().map(resolution -> resolution.getAction()).collect(toList());
+        return l;
+    }
+
+    @GetMapping("/accueil")
+    public String getResolutions(Model model){
+        var resolutions = resolutionService.getAllResolutions();
+        model.addAttribute("resolutions", resolutions);
+
+        return "resolution";
+    }
 }
