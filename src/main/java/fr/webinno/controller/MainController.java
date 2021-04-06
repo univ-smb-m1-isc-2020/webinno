d43d735f371878adcd6f6f240fc26c50e2ad2b3c
benjaminglt1 <benjamin.gallet@etu.univ-savoie.fr>
2021-04-06 15:42:27 +0200

    bug fix

--- src/main/java/fr/webinno/controller/MainController.java
+++ src/main/java/fr/webinno/controller/MainController.java
@@ -1,36 +1,28 @@
 package fr.webinno.controller;
 
 import fr.webinno.domain.Resolution;
-import fr.webinno.service.ResolutionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
 @Controller
 public class MainController {
-    private final ResolutionService resolutionService;
-    private List<Resolution> resolutionService.
-
-    @Autowired
-    public MainController(ResolutionService resolutionService) {
-        this.resolutionService = resolutionService;
-    }
-
-    public List<String> resolutions(){
-        List<String> l = resolutionService.getAllResolutions().stream().map(resolution -> resolution.getAction()).collect(toList());
-        return l;
+    private static List<Resolution> resolutions = new ArrayList<Resolution>();
+    static {
+        resolutions.add(new Resolution("Se coucher avant 20H", "3/mois", 2));
+        resolutions.add(new Resolution("Se coucher avant 21H", "6/mois", 3));
+        resolutions.add(new Resolution("Se coucher avant 22H", "15/an", 6));
+        resolutions.add(new Resolution("Se coucher avant 23H", "1/jour", 1));
     }
 
     @GetMapping("/accueil")
     public String getResolutions(Model model){
-        var resolutions = resolutionService.getAllResolutions();
         model.addAttribute("resolutions", resolutions);
-
         return "resolution";
     }
 }
