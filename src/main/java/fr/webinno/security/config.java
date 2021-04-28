609362534da19e76e064f2b4edac669a6e31ad2d
benjaminglt1 <benjamin.gallet@etu.univ-savoie.fr>
2021-04-28 16:42:48 +0200

    security

--- src/main/java/fr/webinno/security/config.java
+++ src/main/java/fr/webinno/security/config.java
@@ -12,40 +12,47 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class config extends WebSecurityConfigurerAdapter {
+        @Autowired
+        PasswordEncoder passwordEncoder;
 
-    @Autowired
-    @Qualifier("customUserDetailsService")
-    private UserDetailsService customUserDetailsService;
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .passwordEncoder(passwordEncoder)
+                    .withUser("Gallet").password(passwordEncoder.encode("123456789")).roles("USER")
+                    .and()
+                    .withUser("admin").password(passwordEncoder.encode("123456")).roles("USER", "ADMIN");
+        }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests().antMatchers("/","/login","/createAccount").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin().loginPage("/tryLogin").defaultSuccessUrl("/").failureUrl("/login").permitAll()
-                .and().logout().deleteCookies("JSESSIONID").logoutUrl("/logout").logoutSuccessUrl("/");
-    }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
-        authManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder());
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/login","/","/logout")
+                    .permitAll()
+                    .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/tryLogin")
+                    .failureUrl("/login")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .permitAll()
+                    .and()
+                    .csrf()
+                    .disable();
+        }
 
 
 }
